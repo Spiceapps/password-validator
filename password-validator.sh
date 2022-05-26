d6d8f247f3c65e438ea88543dd9b9a136549356d
Spicesapps <evilspice@gmail.com>
2022-05-26 14:09:41 +0300

    all new code, still just basic functionality

--- password-validator.sh
+++ password-validator.sh
@@ -1,15 +1,13 @@
-read -p "please enter password:" password
-count=`echo ${#password}`
-if [[ $count -lt 8 ]];then
-    echo "Password length should be at least 8 charactore"
-exit 1;
-fi
-#echo $password | grep "[A-Z]" | grep "[a-z]" | grep "[0-9]" | grep "[@#$%^&*]"
-#echo $password | grep [A-Za-z][0-9] | grep "[!@#$%^&*()]"
-if [[ ${#password} -ge 6 && "$password" == *[[:lower:]]* && "$password" == *[[:upper:]]* && "$password" == *[0-9]* ]]; then 
-    echo "password is good"  
+read -p "Please enter password:" password   #User inputs password
+
+if [ "${#password}" -ge 10 ] &&             #Check for length
+   [[ $password == *[[:lower:]]* ]] &&      #check lower case
+   [[ $password == *[[:upper:]]* ]] &&      #check uppercase
+   [[ $password == *[!@\#$%^\&*()]* ]]      #check special characters
+then
+    echo 'good'
     exit 0
-else 
-    echo "password is bad"
+else
+    echo 'not good'
     exit 1
 fi
