eb7d654ab1cc01b1e5c75b3d94bcda8d640b0e31
Spicesapps <evilspice@gmail.com>
2022-05-26 15:06:48 +0300

    rewrite to get password as argument, and restructure checks to provide feedback

--- password-validator.sh
+++ password-validator.sh
@@ -1,10 +1,41 @@
-read -p "Please enter password:" password   #User inputs password
+#echo "Password: $1" 
+password=$1
 
-if [ "${#password}" -ge 10 ] &&             #Check for length
-   [[ $password == *[[:lower:]]* ]] &&      #check lower case
-   [[ $password == *[[:upper:]]* ]] &&      #check uppercase
-   [[ $password == *[!@\#$%^\&*()]* ]]      #check special characters
-then
+goodpassword=3
+
+if [ "${#password}" -ge 10 ]; then #Check for length
+    echo "passed length"
+    goodpassword=1
+    else
+         echo "$(tput setaf 1)Weak Password! It should have at least 10 characters."
+         exit 1
+fi
+
+if [[ $password =~ [[:lower:]] ]]; then  #check lower case    
+    echo "passed lower case"   
+    goodpassword=1
+    else 
+    echo "$(tput setaf 1)Weak Password! It should include at least 1 lower case character."
+    exit 1
+fi
+
+if [[ $password =~ [[:upper:]] ]]; then #check uppercase
+          echo "passed upper case"
+          goodpassword=1
+    else
+    echo "$(tput setaf 1)Weak Password! It should include at least 1 upper case character."
+    exit 1
+fi      
+
+if [[ $password =~ [!@\#$%^\&*()] ]]; then #check special characters
+    echo "passed special case"
+    goodpassword=1
+    else
+    echo "$(tput setaf 1)Weak Password! It should include at least 1 special character."
+    exit 1
+fi   
+
+if [[ "$goodpassword" = 1 ]]; then
     echo "$(tput setaf 2)Good Password"
     exit 0
 else
